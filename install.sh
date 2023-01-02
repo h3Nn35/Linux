52ec134f778b73a7f90dd31285470229bf862c99
Christian Hennes <78818014+h3Nn35@users.noreply.github.com>
2023-01-02 15:32:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,4 @@ rm .vimrc
 wget https://raw.githubusercontent.com/h3Nn35/Linux/master/bash_aliases -O .bash_aliases
 wget https://raw.githubusercontent.com/h3Nn35/Linux/master/bashrc -O .bashrc
 wget https://raw.githubusercontent.com/h3Nn35/Linux/master/vimrc -O .vimrc
+rm install.sh
