090f783f72d5165949578f0a18b61d6a1813dbdf
Christian Hennes <78818014+h3Nn35@users.noreply.github.com>
2023-01-02 15:27:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@
 # Use sudo chmod +x install.sh to make it executable
 # install.sh can be executed by using ./install.sh
 
+rm .bash_aliases
 rm .bashrc
+rm .vimrc
 wget https://raw.githubusercontent.com/h3Nn35/Linux/master/bash_aliases -O .bash_aliases
 wget https://raw.githubusercontent.com/h3Nn35/Linux/master/bashrc -O .bashrc
 wget https://raw.githubusercontent.com/h3Nn35/Linux/master/vimrc -O .vimrc
