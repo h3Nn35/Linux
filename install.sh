cdbf337c0f5470719e47187d37de2d74ab3af62e
Christian Hennes <78818014+h3Nn35@users.noreply.github.com>
2023-01-02 15:45:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+# Falls .bash_aliases bereits existiert wird ein Backup erstellt
 if [ -e ~/.bash_aliases ]; then
   mv .bash_aliases .bash_aliases.bak
+  echo "Backup von .bash_aliases erstellt"
 fi
 
+# Falls .bashrc bereits existiert wird ein Backup erstellt
 if [ -e ~/.bashrc ]; then
   mv .bashrc .bashrc.bak
+  echo "Backup von .bashrc erstellt"
 fi
 
+# Falls .vimrc bereits existiert wird ein Backup erstellt
 if [ -e ~/.vimrc ]; then
   mv .vimrc .vimrc.bak
+  echo "Backup von .vimrc erstellt"
 fi
+
 wget https://raw.githubusercontent.com/h3Nn35/Linux/master/bash_aliases -O .bash_aliases
 wget https://raw.githubusercontent.com/h3Nn35/Linux/master/bashrc -O .bashrc
 wget https://raw.githubusercontent.com/h3Nn35/Linux/master/vimrc -O .vimrc
